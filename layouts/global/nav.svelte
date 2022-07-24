44ab42d94e61633b14d9557a6669b9a3888fa253
jimafisk <jimafisk@gmail.com>
2022-07-24 12:28:47 -0400

    Fix mobile menu.

--- layouts/global/nav.svelte
+++ layouts/global/nav.svelte
@@ -1,5 +1,9 @@
 <script>
-  let y;
+  let y, navActive;
+
+  const toggleMenu = () => {
+    navActive = !navActive;
+  }
 </script>
 
 <svelte:window bind:scrollY={y}/>
@@ -7,7 +11,7 @@
 <nav class="navbar {y > 20 ? ' sticky' : ''}">
     <div class="max-width">
         <div class="logo"><a href=".">Portfo<span>lio.</span></a></div>
-        <ul class="menu">
+        <ul class="menu{navActive ? ' active' : ''}">
             <li><a href="#home" class="menu-btn">Home</a></li>
             <li><a href="#about" class="menu-btn">About</a></li>
             <li><a href="#services" class="menu-btn">Services</a></li>
@@ -15,8 +19,8 @@
             <li><a href="#teams" class="menu-btn">Teams</a></li>
             <li><a href="#contact" class="menu-btn">Contact</a></li>
         </ul>
-        <div class="menu-btn">
-            <i class="fas fa-bars"></i>
+        <div class="menu-btn" on:click={toggleMenu}>
+          <i class="fas fa-bars{navActive ? ' active' : ''}"></i>
         </div>
     </div>
 </nav>
