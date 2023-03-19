9fdf1a635b7a7b5c5cdf53a1b0f869fed228bdba
jimafisk <jimafisk@gmail.com>
2023-03-19 15:27:36 -0400

    CMS + v0.6.x upgrade.

--- layouts/global/head.svelte
+++ layouts/global/head.svelte
@@ -7,16 +7,16 @@
   <meta name='viewport' content='width=device-width,initial-scale=1'>
 
   <title>{ title }</title>
-  <base href="{ env.local ? '/' : env.baseurl }">
+  <base href="{env.baseurl}">
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <script type="module" src="spa/ejected/main.js"></script>
+  <script type="module" src="spa/core/main.js"></script>
 
-  <link rel="icon" type="image/svg+xml" href="assets/logo.svg">
+  <link rel="icon" type="image/svg+xml" href="logo.svg">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-  <link rel='stylesheet' href='assets/global.css'>
+  <link rel='stylesheet' href='global.css'>
   <link rel='stylesheet' href='spa/bundle.css'>
 </head>
--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -1,9 +1,17 @@
 <script>
+  import { onMount } from 'svelte';
   import Footer from './footer.svelte';
   import Head from './head.svelte';
   import Nav from './nav.svelte';
+  import Login from './login.svelte';
  
-  export let content, layout, allLayouts, env;
+  export let content, layout, allLayouts, env, user;
+
+  let hash;
+  onMount(async () => {
+    hash = window.location.hash;
+  });
+
   let y;
 </script>
 
@@ -12,13 +20,19 @@
 <html lang="en">
   <Head title={content.filename} {env} />
   <body>
-    <div class="scroll-up-btn {y > 500 ? ' show' : ''}" on:click={() => window.scrollTo(0,0)}>
-      <i class="fas fa-angle-up"></i>
-    </div>
-    <header class="navigation">
-      <Nav /> 
-    </header>
-    <svelte:component this={layout} {...content.fields} {allLayouts} {content} />
-    <Footer />
+    {#if user && $user.isAuthenticated}
+      <svelte:component this={$user.menu} {user} bind:content={content} />
+    {/if}
+    <Login bind:hash {user} />
+    <main>
+      <div class="scroll-up-btn {y > 500 ? ' show' : ''}" on:click={() => window.scrollTo(0,0)}>
+        <i class="fas fa-angle-up"></i>
+      </div>
+      <header class="navigation">
+        <Nav /> 
+      </header>
+      <svelte:component this={layout} {...content.fields} {allLayouts} {content} />
+      <Footer />
+    </main>
   </body>
 </html>
